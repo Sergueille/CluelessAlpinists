7bf20854b6f512d76ca7e474f4ac462d7dcaac96
Sergueille Bismuth <>
2025-09-13 17:36:46 +0200

    Fixed button ignoring clicks

--- Assets/Scripts/ButtonAnimator.cs
+++ Assets/Scripts/ButtonAnimator.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ButtonAnimator : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class ButtonAnimator : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
 {
     [SerializeField] private Image img;
 
     [SerializeField] private float clickDuration;
     [SerializeField] private float bounceDuration;
     [SerializeField] private float scaleAmount;
+
+    private Vector2 pointerInitialPosition;
+    private bool pointerLeftButton;
 
     private void Bounce()
     {
@@ -22,10 +25,23 @@ public class ButtonAnimator : MonoBehaviour, IPointerDownHandler, IPointerUpHand
     {
         LeanTween.cancel(img.gameObject);
         LeanTween.scale(img.gameObject, new Vector3(1 - scaleAmount / 2, 1 - scaleAmount, 1), clickDuration).setIgnoreTimeScale(true);
+
+        pointerInitialPosition = Input.mousePosition;
+        pointerLeftButton = false;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         Bounce();
+
+        if (pointerLeftButton && pointerInitialPosition == (Vector2)Input.mousePosition) // Trigger click even if the poiner is outside (since the button shrunk)
+        {
+            gameObject.GetComponent<Button>().onClick.Invoke();
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        pointerLeftButton = true;
     }
 }
