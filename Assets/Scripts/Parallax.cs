164e7a1276b80ace0f25f68aa8e9d28a266961bd
Sergueille Bismuth <>
2025-09-13 21:28:42 +0200

    Visual changes + map 2 easier

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -5,9 +5,16 @@ public class Parallax : MonoBehaviour
 {
     public float amount;
 
+    public Vector2 startPosition;
+
+    private void Start()
+    {
+        startPosition = gameObject.transform.position;
+    }
+
     private void Update()
     {
-        Vector3 targetPos = CameraController.i.mainCamera.transform.position * amount;
+        Vector3 targetPos = CameraController.i.mainCamera.transform.position * amount + (Vector3)startPosition;
         targetPos.z = 0;
         transform.position = targetPos;
     }
